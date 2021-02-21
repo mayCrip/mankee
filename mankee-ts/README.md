518953bec721d7521045c780866634ea66cbf3fd
Igor Boyarovich <infaction41@gmail.com>
2021-02-21 12:14:35 +0300

    Documentation added

--- mankee-ts/README.md
+++ mankee-ts/README.md
@@ -1,2 +1,21 @@
 # Mankee
 Yet another flashcards app
+
+`yarn dev` to start development server
+
+`yarn build` && `yarn start` for production build
+
+## A Stack
+* next.js
+* prisma
+* React
+* styled-components
+* Docker (currently is not implemented)
+
+## Database setup
+* Configure connection string inside `.env` file
+* Create or update DB schema in `./prisma/schema.prisma`
+* Run a migration via a terminal by executing a command
+```bash
+yarn prisma migrate dev --name <migration_name> --preview-feature
+```
--- mankee-ts/pages/decks/new.tsx
+++ mankee-ts/pages/decks/new.tsx
@@ -1,41 +1,5 @@
-import Router from 'next/router'
-import { useFormik } from 'formik'
 import styled from 'styled-components'
 import Layout from '../../components/Layout'
-
-interface DeckValues {
-    name: string,
-}
-
-const NewDeckForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            name: ''
-        },
-        onSubmit: async (values: DeckValues) => {
-            await fetch('/api/decks', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(values),
-            })
-            Router.push('/')
-        }
-    });
-
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-                id="name"
-                name="name"
-                type="text"
-                onChange={formik.handleChange}
-                value={formik.values.name}
-            />
-            <button type="submit">Add</button>
-        </form>
-    )
-}
 
 const HeaderInput = styled.input`
   border: none;
